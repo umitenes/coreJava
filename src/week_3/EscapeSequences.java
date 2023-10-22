796815f9e87e6a1ae36a736b96688b0c68275ff0
umitenes <akdag.umit@ichc.ro>
2023-10-22 19:57:23 +0300

    week4 commit

--- src/week_3/EscapeSequences.java
+++ src/week_3/EscapeSequences.java
@@ -2,19 +2,9 @@ package week_3;
 
 public class EscapeSequences {
     public static void main(String[] args) {
-        /*
-        Escape sequences: MUST be given with double quote
-             \n : starts a new line
-             \t : paragraph space(tab)
-             \\ : single back slash
-             \" : double quote
-         */
 
-        System.out.println("Hello \n Java class");
-        System.out.println("\t\t more space");
-        System.out.println("\t\t more space");
-        System.out.println("how can you display back slash \\");
-        System.out.println("\"display double quote\"");
-        System.err.println("Gives you messages with red color");
+
+
+
     }
 }
---
+++ src/week_4/Variables.java
@@ -0,0 +1,15 @@
+package week_4;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int salary=120000;   //120_000;
+        long populationOfTheWorld=8_000_000_000L;
+        float diameter=13.45f;
+        double pi=3.14;
+
+
+
+
+    }
+}
