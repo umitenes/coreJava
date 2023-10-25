6ffbe8623ebcd9723c53a7fe84fb81d2b01ec69b
umit <umitakdag@gmail.com>
2023-10-25 15:03:23 +0300

    updated 25 oct

--- src/week_4/Variables.java
+++ src/week_4/Variables.java
@@ -3,18 +3,33 @@ package week_4;
 public class Variables {
     public static void main(String[] args) {
 
-        int salary=120000;   //120_000;
+        int maas=120000;   //120_000;
         long populationOfTheWorld=8_000_000_000L;
         float diameter=13.45f;
         double pi=3.14;
 
         System.out.println("args = " + args);
 
-        System.out.println("hello"+salary);
-        System.out.println(salary+pi+"ter"+diameter);
+        System.out.println("hello"+maas);
+        System.out.println(maas+pi+"ter"+diameter);
 
+        int num;
+        double val=32.33;
+        num=(int)val;
+        System.out.println("num =\' " + num);
 
+       long x=9_999_999_999l;
+       int y= (int) x;
+        System.out.println(y);
 
+        int a=127;
+        byte b=(byte)a;
+        System.out.println(56d);
+        System.out.println("a = " + a);
 
+        int w;
+        byte z=100;
+        int ab=z;
+        
     }
 }
