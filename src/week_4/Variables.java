aad0f89881e9398e0a4302c39c8b826bdf10560b
umitakdag <umitakdag@gmail.com>
2023-11-08 14:45:14 +0200

    week 4 review

--- src/week_4/Variables.java
+++ src/week_4/Variables.java
@@ -3,17 +3,15 @@ package week_4;
 public class Variables {
     public static void main(String[] args) {
 
-        int salary=120000;   //120_000;
-        long populationOfTheWorld=8_000_000_000L;
-        float diameter=13.45f;
-        double pi=3.14;
+        int salary = 120000;   //120_000;
+        long populationOfTheWorld = 8_000_000_000L;
+        float diameter = 13.45f;
+        double pi = 3.14;
 
         System.out.println("args = " + args);
 
-        System.out.println("hello"+salary);
-        System.out.println(salary+pi+"ter"+diameter);
-
-
+        System.out.println("hello" + salary);
+        System.out.println(salary + pi + "ter" + diameter);
 
 
     }
