ede6ae67484f0f2d5c6c30edcf6f503d6a6c32ff
umitakdag <umitakdag@gmail.com>
2023-11-17 08:53:01 +0200

    week 5 if

--- src/week_5/MinNum.java
+++ src/week_5/MinNum.java
@@ -4,9 +4,12 @@ public class MinNum {
     public static void main(String[] args) {
         int num1=100, num2=200;
 
-        if(num1<num2) System.out.println(num1+" is minimum");
-        else if (num2<num1) System.out.println(num2+" is minimum");
-        else System.out.println(num1 + " "+ num2+" are equal");
+        if(num1<num2)
+            System.out.println(num1+" is minimum");
+        else if (num2<num1)
+            System.out.println(num2+" is minimum");
+        else
+            System.out.println(num1 + " "+ num2+" are equal");
 
     }
 }
