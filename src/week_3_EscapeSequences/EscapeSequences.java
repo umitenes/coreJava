13d36d3bb242ba703687c2b6891df1ae0f5ef819
umitakdag <umitakdag@gmail.com>
2023-11-26 23:35:28 +0200

    week 7,updates

--- src/week_3_EscapeSequences/EscapeSequences.java
+++ src/week_3_EscapeSequences/EscapeSequences.java
@@ -3,6 +3,7 @@ package week_3_EscapeSequences;
 public class EscapeSequences {
     public static void main(String[] args) {
 
+        System.out.println("\tMy favorite music is \"Rock\" musics. \n\t\t\t\tMy favorite book is \"Data Structure in Java\"");
 
 
 
---
+++ src/week_3_EscapeSequences/Println_VS_Print.java
@@ -0,0 +1,26 @@
+package week_3_EscapeSequences;
+
+public class Println_VS_Print {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello ICHC");
+        System.out.println("How are you all today?");
+
+        System.out.println("-----------------------------");
+
+        System.out.print("Hello ICHC");
+        System.out.print("How are you all today?");
+
+        System.out.println("Java Programming");
+
+        System.out.print("Wooden Spoon");
+
+        System.out.println("I love Java");
+
+
+
+
+    }
+
+}
