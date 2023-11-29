8c426deb39f103b540b94466cc00f1386d72da4b
umitakdag <umitakdag@gmail.com>
2023-11-29 14:46:57 +0200

    week 6 ternaries

--- src/week_7_Switch_Scanner/Calc.java
+++ src/week_7_Switch_Scanner/Calc.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        System.out.println("pls insert a number");
+
         System.out.println(input.nextInt());
+
+        int x=input.nextInt();
+        System.out.println(x);
+
         System.out.println(input.nextByte());
-        System.out.println(input.nextBoolean());
-        input.close();
-        
+
+        input.nextInt();
+
     }
 }
