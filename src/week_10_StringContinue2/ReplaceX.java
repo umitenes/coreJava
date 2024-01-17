6c8e622367aa64d03f34ee19b156f426d778e1aa
umit <umitakdag@gmail.com>
2024-01-17 16:47:42 +0200

    week 11 whileloop

---
+++ src/week_10_StringContinue2/ReplaceX.java
@@ -0,0 +1,20 @@
+package week_10_StringContinue2;
+public class ReplaceX {
+    public static void main(String[] args) {
+        String str = "xcodeX";
+
+        String result = str.replace("x", "a").replace("X", "a");
+        //  "acodeX"                                 "acodea"
+
+        System.out.println(result);
+
+    }
+
+}
+/*
+2. Write a program that asks user to enter a word. and replace all the x or X with character a
+                    Input:
+                        xcodeX
+                    Output:
+                        acodea
+ */
--- src/week_10_StringContinue2/StringMethod4.java
+++ src/week_10_StringContinue2/StringMethod4.java
@@ -1,9 +1,43 @@
 package week_10_StringContinue2;
 
+import java.util.Scanner;
+
 public class StringMethod4 {
     public static void main(String[] args) {
         String str1="Ichc";
         String str2="ichc";
+        String str3=str1.repeat(5);
+        System.out.println("str3 = " + str3);
+        System.out.println(str2.repeat(10));
+        boolean check=str1.equals(str2);
+        System.out.println("check = " + check);
+        String str5="Ichc";
+        System.out.println(str1 == str5);
+
+        String str7=new String("java");
+        String str8="java";
+        System.out.println(str7.equals(str8));
+        System.out.println(str7 == str8);
+
+        String username="umit";
+        String password="Ichc@2024";
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter user name:");
+        String inputUserName=input.nextLine();
+        input.nextLine();
+        System.out.print("Enter password:");
+        String inputPassword=input.nextLine();
+
+        if (inputUserName.equalsIgnoreCase(username))
+        {
+            if (inputPassword.equals(password))
+            {
+                System.out.println("You're in");
+            }
+        }
+
+
 
 
 
