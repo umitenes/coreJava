d04a9171f3c2f8a167d9cf826749469fef3c329e
umit <umitakdag@gmail.com>
2024-01-31 14:38:37 +0200

    week 11 rectangle

---
+++ src/week_11_While_DoWhile/rectangle.java
@@ -0,0 +1,17 @@
+package week_11_While_DoWhile;
+public class rectangle {
+    public static void main(String[] args) {
+        int x=1, y=8;
+        while(x<=y){
+            System.out.println("******");
+            x++;
+        }
+        System.out.println("_______________________");
+        for (x=1; x<=y; x++){
+            System.out.println("******");
+        }
+
+
+
+    }
+}
