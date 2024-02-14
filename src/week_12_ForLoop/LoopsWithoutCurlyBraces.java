2d11f0844c9d9c695e3f75f0f74e63cc41c94e53
umit <akdag.umit@ichc.ro>
2024-02-14 10:28:38 +0200

    week 12 loops

---
+++ src/week_12_ForLoop/LoopsWithoutCurlyBraces.java
@@ -0,0 +1,19 @@
+package week_12_ForLoop;
+
+public class LoopsWithoutCurlyBraces {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++)
+            System.out.println("Cydeo");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Batch 25");
+        }
+
+
+
+
+    }
+
+}
